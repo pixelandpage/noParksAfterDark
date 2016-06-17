185957c3db7c17b7c71d79e668d92402c236f920
pixelandpage <mags@pixelandpage.co.uk>
2016-06-17 17:58:50 +0100

    in progress

--- www/js/factories/MapFactory.js
+++ www/js/factories/MapFactory.js
@@ -14,7 +14,7 @@ noParks.factory('MapFactory', ["$cordovaGeolocation", function($cordovaGeolocati
     // Instantiate (and display) a map object:
 
 
-  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  // var posOptions = {timeout: 10000, enableHighAccuracy: false};
 
 
     // console.log('outside' + lat);
@@ -46,52 +46,75 @@ noParks.factory('MapFactory', ["$cordovaGeolocation", function($cordovaGeolocati
   //     // error
   //   });
 
-function callGeolocatorLat(){
-  $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-    var lat  = position.coords.latitude;
-    console.log(lat);
-    return lat;
-    // return {'long' : long, 'lat': lat};
-  }, function(err) {
-    // error
-  });
-}
+// function Coords(lat, long){
+//   this.lat = lat;
+//   this.long = long;
+//   console.log('coords function called');
+// }
+//
+// var myPosition = new Coords();
+//
+//   $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+//     var lat  = position.coords.latitude;
+//     var long = position.coords.longitude;
+//     myPosition.long = 10;
+//     myPosition.lat = 60;
+//
+//     // console.log(lat);
+//     // return lat;
+//     // return {'long' : long, 'lat': lat};
+//   }, function(err) {
+//     // error
+//   });
 
-function callGeolocatorLong(){
-  $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
 
-    var long = position.coords.longitude;
-    console.log(long);
-    return long;
-    // return {'long' : long, 'lat': lat};
-  }, function(err) {
-    // error
-  });
-}
+// function callGeolocatorLong(){
+//   $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+//
+//     console.log(long);
+//     return long;
+//     // return {'long' : long, 'lat': lat};
+//   }, function(err) {
+//     // error
+//   });
+// }
 
-function createMap(){
-  // console.log(long);
-  callGeolocatorLat();
-  callGeolocatorLong();
+// function createMap(){
+//   // console.log(long);
+//   callGeolocatorLat();
+//   callGeolocatorLong();
+
     var map = new H.Map(
       document.getElementById('mapContainer'),
       maptypes.normal.map,
       {
         zoom: 10,
-        center: { lng: long, lat: lat }
+        center: { lng: '50', lat: '0' }
         // center: { lng: 25.27, lat: 54.68 } <-- Lithuania Co-ordinates
       });
+
       var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
-      map.addEventListener('tap', function(evt) {
-        console.log(evt.type, evt.currentPointer.type);
-      });
+      // map.addEventListener('tap', function(evt) {
+      //   console.log(evt.type, evt.currentPointer.type);
+      //   map.setCenter(evt.target.getPosition());
+      //   //event target is the marker itself, group is a parent event target
+      //   var bubble = new H.ui.InfoBubble(evt.target.getPosition(),{
+      //     // read custom data
+      //     content: evt.target.getData()
+      //     });
+      //     //show info bubble
+      //     ui.addBubble(bubble);
+      //   }, false);
+      //     addMarkerToGroup(group, position, inf);
 
       var ui = H.ui.UI.createDefault(map, maptypes);
+          return map;
 
-      return map;
-    }
 
-  createMap();
+
+    // }
+
+  // createMap();
 
 }]);
