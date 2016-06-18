9fad953de58aeec951390b468c9465020dc1096f
pixelandpage <mags@pixelandpage.co.uk>
2016-06-18 19:50:17 +0100

    in progress, injecting coords into map to render

--- www/js/factories/MapFactory.js
+++ www/js/factories/MapFactory.js
@@ -1,8 +1,8 @@
 noParks.factory('MapFactory', ["$cordovaGeolocation", function($cordovaGeolocation){
   var app_code ='pG2gTxRQDbxVAsdDMCN1WA';
-   var app_id = 'toJMr8CRe6wBffMtHC4B';
+  var app_id = 'toJMr8CRe6wBffMtHC4B';
 
- var platform = new H.service.Platform({
+  var platform = new H.service.Platform({
     'app_id': app_id,
     'app_code': app_code
     });
@@ -11,86 +11,34 @@ noParks.factory('MapFactory', ["$cordovaGeolocation", function($cordovaGeolocati
     // Obtain the default map types from the platform object
     var maptypes = platform.createDefaultLayers();
 
-    // Instantiate (and display) a map object:
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+
+function test(){
+    $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+      lat  = position.coords.latitude;
+      long = position.coords.longitude;
+      return {lng:long.toString(), lat:lat.toString()};
+      // console.log(mycoords);
+      // return mycoords;
+      // return '{ lng: \'25.27\', lat: \'54.68\'}';
+    }, function(err) {
+      // error
+    });
+}
 
 
-  // var posOptions = {timeout: 10000, enableHighAccuracy: false};
-
-
-    // console.log('outside' + lat);
-    // console.log('outside' + long);
-
-  // var watchOptions = {
-  //   timeout : 3000,
-  //   enableHighAccuracy: false // may cause errors if true
-  // };
-  //
-  // var watch = $cordovaGeolocation.watchPosition(watchOptions);
-  // watch.then(
-  //   null,
-  //   function(err) {
-  //     // error
-  //   },
-  //   function(position) {
-  //     var lat  = position.coords.latitude;
-  //     var long = position.coords.longitude;
-  // });
-
-
-  // watch.clearWatch();
-  // // OR
-  // $cordovaGeolocation.clearWatch(watch)
-  //   .then(function(result) {
-  //     // success
-  //     }, function (error) {
-  //     // error
-  //   });
-
-// function Coords(lat, long){
-//   this.lat = lat;
-//   this.long = long;
-//   console.log('coords function called');
-// }
-//
-// var myPosition = new Coords();
-//
-//   $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-//     var lat  = position.coords.latitude;
-//     var long = position.coords.longitude;
-//     myPosition.long = 10;
-//     myPosition.lat = 60;
-//
-//     // console.log(lat);
-//     // return lat;
-//     // return {'long' : long, 'lat': lat};
-//   }, function(err) {
-//     // error
-//   });
-
-
-// function callGeolocatorLong(){
-//   $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-//
-//     console.log(long);
-//     return long;
-//     // return {'long' : long, 'lat': lat};
-//   }, function(err) {
-//     // error
-//   });
-// }
-
-// function createMap(){
-//   // console.log(long);
-//   callGeolocatorLat();
-//   callGeolocatorLong();
-
+// console.log($cordovaGeolocation.getCurrentPosition(posOptions));
+// console.log($cordovaGeolocation.getCurrentPosition(posOptions.value));
+// console.log($cordovaGeolocation.getCurrentPosition(posOptions.coords));
+    mycoords = test();
+    console.log(mycoords);
     var map = new H.Map(
       document.getElementById('mapContainer'),
       maptypes.normal.map,
       {
         zoom: 10,
-        center: { lng: '50', lat: '0' }
-        // center: { lng: 25.27, lat: 54.68 } <-- Lithuania Co-ordinates
+        // center: $cordovaGeolocation.getCurrentPosition()
+        center: { lng: '0', lat: '0' }
       });
 
       var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
@@ -110,11 +58,5 @@ noParks.factory('MapFactory', ["$cordovaGeolocation", function($cordovaGeolocati
 
       var ui = H.ui.UI.createDefault(map, maptypes);
           return map;
-
-
-
-    // }
-
-  // createMap();
 
 }]);
