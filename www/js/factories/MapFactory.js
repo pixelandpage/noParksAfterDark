ac696cbe244e4a821b7af13fef1c64b1088cfc42
pixelandpage <mags@pixelandpage.co.uk>
2016-06-18 20:16:28 +0100

    temporary solution to injecting current location's coords into ionic working

--- www/js/factories/MapFactory.js
+++ www/js/factories/MapFactory.js
@@ -2,46 +2,38 @@ noParks.factory('MapFactory', ["$cordovaGeolocation", function($cordovaGeolocati
   var app_code ='pG2gTxRQDbxVAsdDMCN1WA';
   var app_id = 'toJMr8CRe6wBffMtHC4B';
 
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
   var platform = new H.service.Platform({
     'app_id': app_id,
     'app_code': app_code
     });
 
     // var targetElement = document.getElementById('mapContainer');
-    // Obtain the default map types from the platform object
     var maptypes = platform.createDefaultLayers();
 
-    var posOptions = {timeout: 10000, enableHighAccuracy: false};
 
-function test(){
-    $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
       lat  = position.coords.latitude;
       long = position.coords.longitude;
-      return {lng:long.toString(), lat:lat.toString()};
-      // console.log(mycoords);
+      var mycoords =  {lng:long.toString(), lat:lat.toString()};
+
+      var map = new H.Map(
+        document.getElementById('mapContainer'),
+        maptypes.normal.map,
+        {
+          zoom: 10,
+          center: mycoords
+        });
+
       // return mycoords;
-      // return '{ lng: \'25.27\', lat: \'54.68\'}';
+      var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+      var ui = H.ui.UI.createDefault(map, maptypes);
     }, function(err) {
       // error
     });
-}
+    return 'map';
 
 
-// console.log($cordovaGeolocation.getCurrentPosition(posOptions));
-// console.log($cordovaGeolocation.getCurrentPosition(posOptions.value));
-// console.log($cordovaGeolocation.getCurrentPosition(posOptions.coords));
-    mycoords = test();
-    console.log(mycoords);
-    var map = new H.Map(
-      document.getElementById('mapContainer'),
-      maptypes.normal.map,
-      {
-        zoom: 10,
-        // center: $cordovaGeolocation.getCurrentPosition()
-        center: { lng: '0', lat: '0' }
-      });
-
-      var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
       // map.addEventListener('tap', function(evt) {
       //   console.log(evt.type, evt.currentPointer.type);
@@ -56,7 +48,5 @@ function test(){
       //   }, false);
       //     addMarkerToGroup(group, position, inf);
 
-      var ui = H.ui.UI.createDefault(map, maptypes);
-          return map;
 
 }]);
